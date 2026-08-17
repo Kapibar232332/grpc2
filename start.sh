8b7be537b8deaf2c509545d0392ff63a94f86054
Kapibar232332 <kapibarakingurusavic@gmail.com>
2026-08-18 00:09:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,10 +6,7 @@ PORT="${PORT:-10000}"
 
 sed -i "s/\"port\": 10000/\"port\": ${PORT}/" /etc/xray/config.json
 
-echo "================================="
-echo "Xray VLESS gRPC"
-echo "Listening: 0.0.0.0:${PORT}"
+echo "Starting Xray on 0.0.0.0:${PORT}"
 echo "gRPC service: vpn"
-echo "================================="
 
-exec /usr/local/bin/xray run -config /etc/xray/config.json
+exec /usr/local/bin/xray -config /etc/xray/config.json
